240cb59defe9370120bc0252b9ddb24449dc09af
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-14 11:09:50 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+    constructor() {
+        super()
+    }
+
+    focusButton = () => {
+        console.log("Good!")
+    }
+
+    blurButton = () => {
+        console.log("Hey! Eyes on me!")
+    }
+
+    render() {
+        return (
+            <div>
+                <button onFocus={this.focusButton} onBlur={this.blurButton} ></button>
+            </div>
+        )
+    }
+} 
